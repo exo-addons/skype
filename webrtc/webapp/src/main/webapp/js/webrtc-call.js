654e906eec7ee942cc6bc2169ce927d4aecef36c
Peter Nedonosko <pnedonosko@exoplatform.com>
2017-09-06 21:00:51 +0300

    added more STUN servers

--- webrtc/webapp/src/main/webapp/js/webrtc-call.js
+++ webrtc/webapp/src/main/webapp/js/webrtc-call.js
@@ -158,9 +158,23 @@ if (eXo.videoCalls) {
 							// WebRTC connection to establish a call connection
 							log("Creating RTC peer connection for " + callId + " > " + new Date().getTime());
 							var pc = new RTCPeerConnection({
-								iceServers: [{
-									"urls": "stun:stun.l.google.com:19302"
-								}]
+								iceServers: [
+									{ 
+										"urls": [
+											"stun:stun.l.google.com:19302", 
+											"stun:stun1.l.google.com:19302",
+											"stun:stun2.l.google.com:19302",
+											"stun:stun3.l.google.com:19302",
+											"stun:stun4.l.google.com:19302"
+										]
+									}, {
+										"urls": [
+											"stun:stunserver.org",
+											"stun:stun01.sipphone.com",
+											"stun:stun.voiparound.com"
+										]
+									}
+								]
 							});
 							var negotiation = $.Deferred();
 							var connection = $.Deferred();
